f21c0050ac38f4824659dfb3dbb01cf44bb19d44
emgrey02 <emgrey02@gmail.com>
2025-08-23 20:22:57 -0400

    Keep trying, change database url

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'hookneedle.db')
+        DATABASE='./hookneedle.db'
     )
 
     if test_config is None:
